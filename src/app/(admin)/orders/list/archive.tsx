449d6d3e73ad57c5e3bdd2ffe5a2f2b354c2e968
rayl <liuyi86@zto.com>
2025-09-27 22:37:29 +0800

    feat: Admin Orders

---
+++ src/app/(admin)/orders/list/archive.tsx
@@ -0,0 +1,25 @@
+import { FlatList } from "react-native";
+
+// import { useMyOrderList } from "@/api/orders";
+import OrderListItem from "@/components/OrderListItem";
+
+import orders from "@assets/data/orders";
+
+export default function OrdersScreen() {
+  // const { data: orders, isLoading, error } = useMyOrderList();
+
+  // if (isLoading) {
+  //   return <ActivityIndicator />;
+  // }
+  // if (error) {
+  //   return <Text>Failed to fetch</Text>;
+  // }
+
+  return (
+    <FlatList
+      data={orders}
+      renderItem={({ item }) => <OrderListItem order={item} />}
+      contentContainerStyle={{ gap: 10, padding: 10 }}
+    />
+  );
+}
