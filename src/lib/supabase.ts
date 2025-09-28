4220b1ed7570d9a21d13238f9fa335c3213061cd
rayl <liuyi86@zto.com>
2025-09-29 07:47:17 +0800

    feat: 接入supabase

---
+++ src/lib/supabase.ts
@@ -0,0 +1,35 @@
+import { createClient } from "@supabase/supabase-js";
+import { deleteItemAsync, getItemAsync, setItemAsync } from "expo-secure-store";
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    console.debug("getItem", { key, getItemAsync });
+    return getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    if (value.length > 2048) {
+      console.warn(
+        "Value being stored in SecureStore is larger than 2048 bytes and it may not be stored successfully. In a future SDK version, this call may throw an error."
+      );
+    }
+    return setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    return deleteItemAsync(key);
+  },
+};
+
+export const supabase = createClient(
+  process.env.EXPO_PUBLIC_SUPABASE_URL ??
+    "https://acnopaldoudvqyoprilr.supabase.co",
+  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ??
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjbm9wYWxkb3VkdnF5b3ByaWxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwOTg0NzEsImV4cCI6MjA3NDY3NDQ3MX0.JSbvkjY-EfZH_hk6lr3KJA8fMe5KEx2cKObk7Ud8w54",
+  {
+    auth: {
+      storage: ExpoSecureStoreAdapter as any,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
+  }
+);
